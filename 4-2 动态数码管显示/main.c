8d4da69448a2d2389f2bf16a46da1d63abe2ae33
scu_Lee <scu_Lee@outlook.com>
2024-12-19 23:11:43 +0800

    4-2 动态数码管显示

---
+++ 4-2 动态数码管显示/main.c
@@ -0,0 +1,48 @@
+#include <REGX52.H>
+
+unsigned char NixieTable[]={0x3F,0X06,0X5B,0X4F,0X66,0X6D,0X7D,0X07,0X7F,0X6F};//0-9
+
+void Delay(unsigned int xms)
+{
+	unsigned char i, j;
+	while(xms--)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
+
+void Nixie(unsigned char Location,unsigned char Number)
+{
+	switch(Location)
+	{
+		case 1:P2_4=1;P2_3=1;P2_2=1;break;
+		case 2:P2_4=1;P2_3=1;P2_2=0;break;
+		case 3:P2_4=1;P2_3=0;P2_2=1;break;
+		case 4:P2_4=1;P2_3=0;P2_2=0;break;
+		case 5:P2_4=0;P2_3=1;P2_2=1;break;
+		case 6:P2_4=0;P2_3=1;P2_2=0;break;
+		case 7:P2_4=0;P2_3=0;P2_2=1;break;
+		case 8:P2_4=0;P2_3=0;P2_2=0;break;
+	}
+	P0=NixieTable[Number];
+	Delay(1);//∑¿÷π¥ÆŒªœ‘ æ≤–”∞
+	P0=0x00;//
+}
+
+void main()
+{
+	while(1)
+	{
+		Nixie(1,1);
+//	Delay(20);
+		Nixie(2,2);
+//	Delay(20);
+		Nixie(3,3);
+//	Delay(20);
+	}
+}
